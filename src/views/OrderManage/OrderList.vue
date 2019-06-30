12d4d6d01349f42dfccfdbbf50a31efbeba1109b
liuzijiang <lIUZiJiangNiuBi>
2019-06-30 20:46:47 +0800

    XX

--- src/views/OrderManage/OrderList.vue
+++ src/views/OrderManage/OrderList.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="listArea">
 
+
+
     <el-form :inline="true" :model="formInline" class="demo-form-inline">
       <el-row :gutter="24">
         <el-col :sm="{span: 6}" :xs="{span: 23}"  >
           <el-form-item label="订单编号" >
-            <el-input v-model="formInline.user" placeholder="输入编号" ><i slot="prefix" class="el-icon-edit"></i></el-input>
+            <el-input v-model="orderPid" placeholder="输入编号" ><i slot="prefix" class="el-icon-edit"></i></el-input>
           </el-form-item>
         </el-col>
 
         <el-col :sm="{span: 6}" :xs="{span: 23}">
           <el-form-item label="店铺名称">
-            <el-input v-model="formInline.user" placeholder="输入名称"><i slot="prefix" class="el-icon-edit"></i></el-input>
+            <el-input v-model="dianPuNamer" placeholder="输入名称"><i slot="prefix" class="el-icon-edit"></i></el-input>
           </el-form-item>
         </el-col>
 
         <el-col :sm="{span: 6}" :xs="{span: 23}">
           <el-form-item label="订单类型" >
-            <el-select v-model="formInline.region" placeholder="选择" >
+            <el-select v-model="orderCast" placeholder="选择" >
               <el-option label="普通订单" value="普通订单"></el-option>
               <el-option label="优惠订单" value="优惠订单"></el-option>
             </el-select>
@@ -26,7 +28,7 @@
 
         <el-col :sm="{span: 6}" :xs="{span: 23}">
           <el-form-item label="订单状态">
-            <el-select v-model="formInline.region" placeholder="选择">
+            <el-select v-model="orderState" placeholder="选择">
               <el-option label="待付款" value="待付款"></el-option>
               <el-option label="已取消" value="已取消"></el-option>
               <el-option label="未发货" value="未发货"></el-option>
@@ -50,8 +52,8 @@
         <el-col :sm="{span: 6}" :xs="{span: 23}"  >
           <el-form-item label="开始时间" class="myItem">
             <el-date-picker style="width: 100%;"
-                            v-model="value1"
-                            type="datetime"
+                            v-model="startDate"
+                            type="date"
                             placeholder="选择日期时间"
                             default-time="12:00:00">
             </el-date-picker>
@@ -60,8 +62,8 @@
         <el-col :sm="{span: 6}" :xs="{span: 23}"  >
           <el-form-item label="结束时间" class="myItem">
             <el-date-picker style="width: 100%;"
-                            v-model="value2"
-                            type="datetime"
+                            v-model="endDate"
+                            type="date"
                             placeholder="选择日期时间"
                             default-time="12:00:00">
             </el-date-picker>
@@ -132,17 +134,17 @@
           </el-table-column>
           <el-table-column  prop="created_at"   show-overflow-tooltip="true" >
             <template slot="header" slot-scope="scope">
-              <span>商品详情</span>
+              <span>订单商品</span>
               <el-tooltip class="item" effect="dark" content="商品信息描述" placement="right">
                 <svg-icon icon-class="product"  />
               </el-tooltip>
             </template>
             <template slot-scope="scope">
               <el-button
-                @click.native.prevent="showMess(scope.$index, list)"
+                @click.native.prevent="productDesc(scope.$index, list)"
                 type="text"
                 size="20px">
-                商品详情
+                {{ scope.row.product }}
               </el-button>
             </template>
           </el-table-column>
@@ -155,10 +157,10 @@
             </template>
             <template slot-scope="scope">
               <el-button
-                @click.native.prevent="showMess(scope.$index, list)"
+                @click.native.prevent="orderPriceDesc(scope.$index, list)"
                 type="text"
                 size="20px">
-                金额详情
+                {{ scope.row.price }}
               </el-button>
             </template>
           </el-table-column>
@@ -174,7 +176,7 @@
                 @click.native.prevent="showMess(scope.$index, list)"
                 type="text"
                 size="20px">
-                用户详情
+                {{ scope.row.users }}
               </el-button>
             </template>
           </el-table-column>
@@ -190,7 +192,7 @@
                 @click.native.prevent="showMess(scope.$index, list)"
                 type="text"
                 size="20px">
-                查看物流
+                {{ scope.row.logistics }}
               </el-button>
               <span></span>
             </template>
@@ -198,7 +200,6 @@
         </el-table>
       </el-col>
     </el-row>
-
 
     <el-row :gutter="24">
       <el-col :sm="{span: 5, offset: 19}" :xs="{span: 24}">
@@ -215,6 +216,68 @@
         </el-pagination>
       </el-col>
     </el-row>
+
+    <!--===================商品详情描述弹出框(开始)========================-->
+    <el-dialog title="商品详情" :visible.sync="productDescVisible"  width="30%" >
+        <el-table
+          v-loading="listLoading"
+          :data="orderDescList"
+          element-loading-text="Loading"
+          border
+        >
+          <el-table-column class-name="status-col" label="类型"  show-overflow-tooltip="true"  >
+            <template slot-scope="scope">
+              <span>{{ scope.row.cast }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column  prop="created_at" label="信息"  show-overflow-tooltip="true"  >
+            <template slot-scope="scope">
+              <span>{{ scope.row.desc }}</span>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div slot="footer" class="dialog-footer " >
+          <el-button @click="productDescVisible = false">取 消</el-button>
+          <el-button type="primary" @click="productDescVisible = false">确 定</el-button>
+        </div>
+    </el-dialog>
+    <!--===================商品详情描述弹出框(结束)========================-->
+
+
+    <!--===================订单金额详情弹出框(开始)========================-->
+    <el-dialog title="订单金额" :visible.sync="orderPriceVisible"  width="30%" >
+      <el-table
+        v-loading="listLoading"
+        :data="orderPriceList"
+        element-loading-text="Loading"
+        border
+      >
+        <el-table-column class-name="status-col" label="类型"  show-overflow-tooltip="true"  >
+          <template slot-scope="scope">
+            <span>{{ scope.row.cast }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column  prop="created_at" label="信息"  show-overflow-tooltip="true"  >
+          <template slot-scope="scope">
+            <span v-if="scope.$index==6">
+                <el-input v-model="elsePrice"   placeholder="请输入" :value="scope.row.desc"  ></el-input>
+            </span>
+            <span v-if="scope.$index==7">
+                <el-input v-model="elseDesc"   placeholder="请输入" :value="scope.row.desc"  ></el-input>
+            </span>
+            <span v-if="scope.$index!=7&&scope.$index!=6">
+                {{scope.row.desc}}
+            </span>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div slot="footer" class="dialog-footer " >
+        <el-button @click="orderPriceVisible = false">取 消</el-button>
+        <el-button type="primary" @click="orderPriceVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!--===================订单金额详情弹出框(结束)========================-->
+
   </div>
 </template>
 
@@ -222,46 +285,72 @@
   export default {
     data() {
       return {
+        orderDescList:[
+          {cast:"商品编号",desc:"1231241413"},
+          {cast:"店铺名称",desc:"小彭家具"},
+          {cast:"商品名称",desc:"茶杯"},
+          {cast:"商品数量",desc:"2"},
+          {cast:"商品总金额",desc:"78"},
+          {cast:"商品购项",desc:"红色-4g-6+128"},
+        ],
+        orderPriceList:[
+          {cast:"支付编号",desc:"5148949848948"},
+          {cast:"下单时间",desc:"2019-1"},
+          {cast:"商品金额",desc:"110"},
+          {cast:"优惠金额",desc:"20"},
+          {cast:"实付金额",desc:"90"},
+          {cast:"运费",desc:"15"},
+          {cast:"其他金额",desc:"0"},
+          {cast:"其他描述",desc:"无"},
+          {cast:"支付时间",desc:"2019-1-1"},
+          {cast:"支付类型",desc:"支付宝"},
+        ],
+        productDescVisible: false,
+        orderPriceVisible:  false,
         pickerOptions: {
+          disabledDate(time) {
+            return time.getTime() > Date.now()
+          },
           shortcuts: [{
             text: '今天',
             onClick(picker) {
-              picker.$emit('pick', new Date());
+              picker.$emit('pick', new Date())
             }
           }, {
             text: '昨天',
             onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24);
-              picker.$emit('pick', date);
+              const date = new Date()
+              date.setTime(date.getTime() - 3600 * 1000 * 24)
+              picker.$emit('pick', date)
             }
           }, {
             text: '一周前',
             onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', date);
+              const date = new Date()
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+              picker.$emit('pick', date)
             }
           }]
         },
-        value1: '',
-        value2: '',
-        formInline: {
-          user: '',
-          region: ''
-        },
+        startDate: '',
+        endDate: '',
+        orderCast: '',
+        orderState: '',
+        orderPid: '',
+        dianPuNamer: '',
         total: 100,
         currentPage: 2,
-        dialogFormVisible: false,
+        elsePrice:'',
+        elseDesc:'',
         list: [
           {
             pid: '1',
             number: '123123',
-            store: '上海市上海市上海市上海市上',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -269,11 +358,11 @@
           {
             pid: '2',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -281,11 +370,11 @@
           {
             pid: '3',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -293,11 +382,11 @@
           {
             pid: '4',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -305,11 +394,11 @@
           {
             pid: '5',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -317,11 +406,11 @@
           {
             pid: '6',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -329,11 +418,11 @@
           {
             pid: '7',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -341,11 +430,11 @@
           {
             pid: '8',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -353,11 +442,11 @@
           {
             pid: '8',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -365,11 +454,11 @@
           {
             pid: '9',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -377,11 +466,11 @@
           {
             pid: '10',
             number: '123123',
-            store: '上海市上海市上海市上海市',
+            store: '小彭家居',
             date: '2019-9-9',
-            cast: '上海市上海市上海市上海市',
-            state: '王小虎',
-            product: '上海市上海市上海市上海市',
+            cast: '优惠订单',
+            state: '已发货',
+            product: '苹果6x',
             price:123,
             users:'小明',
             logistics:'123123'
@@ -389,12 +478,20 @@
         ]
       }
     },
+
     methods: {
+      //表单提交方法
       onSubmit() {
-        console.log('submit!');
+        console.log('submit!')
       },
-      showMess(currIndex,obj2){
-        alert(obj2[currIndex].users);
+
+      //商品详情方法
+      productDesc(currIndex,obj){
+          this.productDescVisible=true
+      },
+      //订单金额方法
+      orderPriceDesc(currIndex,obj){
+        this.orderPriceVisible=true
       }
     }
   }
@@ -426,16 +523,28 @@
     .el-divider span{
       font-size: 11px;
     }
-    /* .myItem{
-            width: 100% !important;
-        }
-        .myItem .el-form-item__label{
-            width: 66px;
-            padding: 0px;
-            text-align: left;
-        } */
+
+    /*========弹出框样式信息开始============*/
+    .el-dialog__wrapper .el-dialog{
+       width: 80% !important;
+    }
+
+
   }
 
+
+  /*========弹出框样式信息开始============*/
+  .el-dialog__body{
+    padding-top: 10px !important;
+  }
+  .el-dialog__body .el-table td{
+    padding: 6px 0px !important;
+  }
+
+
+
+
+  /*========当前页面的一些初始样式 输入框和表格等标签样式===========*/
   .el-divider span{
     color: #606266;font-weight:bold;
   }
