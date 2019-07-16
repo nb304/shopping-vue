cfbe8db2186ecc17cf230f17fb5970d86991a44c
luqiqi <2246483156@qq.com>
2019-07-16 21:00:51 +0800

    更新了“手机端”聊天系统

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -5,26 +5,36 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+  import {
+    mapGetters
+  } from 'vuex'
 
-export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
+  export default {
+    name: 'Dashboard',
+    computed: {
+      ...mapGetters([
+        'name'
+      ])
+    },
+    created() {
+      this.$notify({
+        title: '注意!!!',
+        message: '请尽量使用电脑打开后台管理系统,手机端适配暂不完全。',
+        duration: 0
+      });
+    }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+  .dashboard {
+    &-container {
+      margin: 30px;
+    }
+
+    &-text {
+      font-size: 30px;
+      line-height: 46px;
+    }
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
 </style>
